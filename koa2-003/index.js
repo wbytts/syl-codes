1fd018668d4ec747cf444bd86a9bc6e0de955db7
wbytts <wbytts@163.com>
2022-08-04 17:19:58 +0800

    参数获取

--- koa2-003/index.js
+++ koa2-003/index.js
@@ -2,12 +2,29 @@ const Koa = require("koa");
 const app = new Koa();
 const router = require("koa-router")();
 
+
 router.get("/", (ctx) => {
+  // 当在浏览器里访问 url/?name=gongmw 地址，通过 ctx.query 就可以获得路径的参数值对象。
+  console.log(ctx.query); //{ name: 'gongmw' }
+  console.log(ctx.querystring); //name=gongmw
   ctx.body = "hello world";
+});
+
+// POST参数获取
+router.post("/submit", (ctx) => {
+  console.log(ctx.request.body);
+  ctx.body = "submit success";
 });
 
 router.get("/404", (ctx) => {
   ctx.body = "页面数据不存在";
+});
+
+// 动态路由参数
+// 当路径是一个动态变化值的时候，可以通过 ctx.params 来获取动态对应的参数。
+router.get("/404/:id", async (ctx) => {
+  console.log(ctx.params); //{ id: '1111' }
+  ctx.body = "数据404";
 });
 
 // 加载路由中间件
